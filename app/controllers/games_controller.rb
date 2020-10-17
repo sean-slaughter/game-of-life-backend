20c019aa4752438e948e0043162f2ff077983b99
sean-slaughter <sthompson2822@gmail.com>
2020-10-17 17:33:30 -0500

    remove unused controller actions

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,13 +8,10 @@ class GamesController < ApplicationController
     render json: @games
   end
 
-  # GET /games/1
-  def show
-    render json: @game
-  end
 
   # POST /games
   def create
+    byebug
     @game = Game.new(game_params)
 
     if @game.save
@@ -24,25 +21,9 @@ class GamesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /games/1
-  def update
-    if @game.update(game_params)
-      render json: @game
-    else
-      render json: @game.errors, status: :unprocessable_entity
-    end
-  end
 
-  # DELETE /games/1
-  def destroy
-    @game.destroy
-  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_game
-      @game = Game.find(params[:id])
-    end
 
     # Only allow a trusted parameter "white list" through.
     def game_params
